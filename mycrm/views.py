b0ecfd3af393e8e16c31f422c790539543a53694
solaleh81 <solalehalizadeh81.com>
2023-10-11 13:41:21 +0330

    adding login to def home.

--- mycrm/views.py
+++ mycrm/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have login successfully!')
+            return redirect('home')
+        else:
+            messages.success(request, "we couldnt find an account with that username!")
+    else:
+        return render(request, 'home.html')
