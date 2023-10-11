e65b3e85c53867f18c884cbfb151487170f7d84b
solaleh81 <solalehalizadeh81.com>
2023-10-11 15:14:42 +0330

    adding def logout view.

--- mycrm/views.py
+++ mycrm/views.py
@@ -1,16 +1,26 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
+
 def home(request):
     if request.method == 'POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        user=authenticate(request, username=username, password=password)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, 'You have login successfully!')
+
+            messages.success(request, 'You have login succesfully!')
             return redirect('home')
         else:
-            messages.success(request, "we couldnt find an account with that username!")
+            messages.success(request,
+                             "We couldn't find an account with that username. Try another, or get a new  account.")
+            return redirect('home')
     else:
         return render(request, 'home.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logout succesfully")
+    return redirect('home')
